dddd1243c816d80a07557cb1dac798b02fff53cd
Chao Cheng <chaocheng@college.harvard.edu>
2021-09-17 15:54:41 -0400

    Pset1

--- pset1/AUTHORS.md
+++ pset1/AUTHORS.md
@@ -3,13 +3,12 @@ Author and collaborators
 
 Student
 -------
-(Your name.)
+Chao Cheng
 
 
 Collaborators
 -------------
-(List any other collaborators and describe help you got from other students
-in the class.)
+Wendy Qiu
 
 
 Citations
--- pset1/m61.cc
+++ pset1/m61.cc
@@ -1,10 +1,47 @@
 #define M61_DISABLE 1
+#define METADATA_LIMIT 20000000
+#define MAGIC_NUMBER 42
 #include "m61.hh"
 #include <cstdlib>
 #include <cstring>
 #include <cstdio>
 #include <cinttypes>
 #include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+// Initialize stats struct
+struct m61_statistics _stats {0, 0, 0, 0, 0, 0, 0, 0};
+
+// Metadata tracking active sizes
+struct metadata {
+    size_t size;
+    const char* file;
+    long line;
+    bool active;
+    metadata() {
+        size = 0;
+        file = nullptr;
+        line = 0;
+        active = true;
+    }
+    metadata(size_t _size, const char* _file, long _line, bool _active) {
+        this->size = _size;
+        this->file = _file;
+        this->line = _line;
+        this->active = _active;
+    }
+};
+
+unordered_map<uintptr_t, metadata> allocated_pointers;
+unordered_map<string, size_t> heavy_hitters;
+queue<uintptr_t> allocated_pointers_q;
+
 
 /// m61_malloc(sz, file, line)
 ///    Return a pointer to `sz` bytes of newly-allocated dynamic memory.
@@ -15,7 +52,54 @@
 void* m61_malloc(size_t sz, const char* file, long line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
     // Your code here.
-    return base_malloc(sz);
+
+    // If size is too large, return nullptr
+    if (sz >= (size_t) - 1){
+        _stats.nfail++;
+        _stats.fail_size += sz;
+        return nullptr;
+    }
+
+    // Allocate
+    void* ptr = base_malloc(sz + 1);
+
+    // Check if allocation failed
+    if (ptr == NULL) {
+        _stats.nfail++;
+        _stats.fail_size += sz;
+        return ptr;
+    }
+
+    // Metadata for detecting boundary write error
+    char* bound = (char*) ((uintptr_t)ptr + sz);
+    *bound = MAGIC_NUMBER;
+
+    // Update stats
+    metadata m(sz, file, line, true);
+    allocated_pointers[(uintptr_t) ptr] = m;
+    allocated_pointers_q.push((uintptr_t) ptr);
+    while (allocated_pointers_q.size() >= METADATA_LIMIT) {
+        allocated_pointers.erase(allocated_pointers_q.front());
+        allocated_pointers_q.pop();
+    }
+    _stats.nactive++;
+    _stats.ntotal++;
+    _stats.total_size += sz;
+    _stats.active_size += sz;
+    if ((uintptr_t) ptr < _stats.heap_min || _stats.heap_min == 0) _stats.heap_min = (uintptr_t) ptr;
+    if ((uintptr_t) ptr + sz - 1 > _stats.heap_max || _stats.heap_max == 0) _stats.heap_max = (uintptr_t) ptr + sz - 1;
+
+    // Update heavy hitters
+    string hh = file;
+    hh.append(":");
+    hh.append(to_string(line));
+    if (heavy_hitters.find(hh) != heavy_hitters.end()) {
+        heavy_hitters[hh] += sz;
+    } else {
+        heavy_hitters[hh] = sz;
+    }
+
+    return ptr;
 }
 
 
@@ -27,7 +111,43 @@ void* m61_malloc(size_t sz, const char* file, long line) {
 void m61_free(void* ptr, const char* file, long line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
     // Your code here.
-    base_free(ptr);
+
+    // Check if null pointer
+    if (ptr == NULL) {
+        return;
+    }
+
+    // Check if pointer exists in allocated_pointers metadata
+    if (allocated_pointers.find((uintptr_t) ptr) != allocated_pointers.end()) {
+        // Check if double free
+        if (!allocated_pointers[(uintptr_t) ptr].active) {
+            fprintf(stderr, "MEMORY BUG: %s:%ld: invalid free of pointer %p, double free\n", file, line, ptr);
+        }
+
+        // Check if boundary write error
+        char* bound = (char*) ((uintptr_t)ptr + allocated_pointers[(uintptr_t) ptr].size);
+        if (*bound != MAGIC_NUMBER) {
+            fprintf(stderr, "MEMORY BUG: %s:%ld: detected wild write during free of pointer %p\n", file, line, ptr);
+            abort();
+        }
+        
+        // Free
+        base_free(ptr);
+
+        // Update stats
+        --_stats.nactive;
+        _stats.active_size -= allocated_pointers[(uintptr_t) ptr].size;
+        allocated_pointers[(uintptr_t) ptr].active = false;
+    } else {
+        for (auto it = allocated_pointers.begin(); it != allocated_pointers.end(); it++) {
+            if ((uintptr_t) ptr > it->first && (uintptr_t) ptr <= it->first + it->second.size) {
+                fprintf(stderr, "MEMORY BUG: %s:%ld: invalid free of pointer %p, not allocated\n", file, line, ptr);
+                fprintf(stderr, "\t%s:%ld: %p is %ld bytes inside a %ld byte region allocated here\n", file, it->second.line, ptr, (uintptr_t) ptr - it->first, it->second.size);
+                return;
+            }
+        }
+        fprintf(stderr, "MEMORY BUG: %s:%ld: invalid free of pointer %p, not in heap\n", file, line, ptr);
+    }
 }
 
 
@@ -40,6 +160,11 @@ void m61_free(void* ptr, const char* file, long line) {
 
 void* m61_calloc(size_t nmemb, size_t sz, const char* file, long line) {
     // Your code here (to fix test019).
+    if ((size_t) -1 / sz < nmemb) {
+        _stats.nfail++;
+        _stats.fail_size += nmemb * sz;
+        return nullptr;
+    }
     void* ptr = m61_malloc(nmemb * sz, file, line);
     if (ptr) {
         memset(ptr, 0, nmemb * sz);
@@ -55,6 +180,7 @@ void m61_get_statistics(m61_statistics* stats) {
     // Stub: set all statistics to enormous numbers
     memset(stats, 255, sizeof(m61_statistics));
     // Your code here.
+    *stats = _stats;
 }
 
 
@@ -78,6 +204,18 @@ void m61_print_statistics() {
 
 void m61_print_leak_report() {
     // Your code here.
+    for (auto it = allocated_pointers.begin(); it != allocated_pointers.end(); it++) {
+        if (it->second.active) {
+            printf("LEAK CHECK: %s:%ld: allocated object %p with size %lu\n", it->second.file, it->second.line, (void*) it->first, it->second.size);
+        }
+    }
+}
+
+
+/// hh_cmp()
+///    Helper function to compare sizes of heavy hitters
+bool hh_cmp(pair<string, size_t> &a, pair<string, size_t> &b) {
+    return a.second > b.second;
 }
 
 
@@ -86,4 +224,15 @@ void m61_print_leak_report() {
 
 void m61_print_heavy_hitter_report() {
     // Your heavy-hitters code here
+    vector<pair<string, size_t>> hh_vector;
+    for (auto it : heavy_hitters) {
+        hh_vector.push_back(it);
+    }
+    sort(hh_vector.begin(), hh_vector.end(), hh_cmp);
+    for (auto it = hh_vector.begin(); it != hh_vector.end(); it++) {
+        double percentage = (double) it->second / _stats.total_size * 100;
+        if (percentage >= 20) {
+            printf("HEAVY HITTER: %s: %lu bytes (~%.1f%%)\n", it->first.substr(2).c_str(), it->second, percentage);
+        }
+    }
 }
